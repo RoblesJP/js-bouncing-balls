435b10943ccf21656bffbdb1cd87a79328c3fe27
RoblesJP <juan.robles1417@gmail.com>
2021-02-27 12:53:30 -0300

    Move Ball class to it's own file

---
+++ script/ball.js
@@ -0,0 +1,110 @@
+class Ball {
+    constructor(positionX, positionY, radius, velocityX, velocityY, mass, color) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.radius = radius;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.mass = mass;
+        this.color = color;
+    }
+
+    show() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.restore();
+    }
+
+    move() {
+        // position update
+        this.positionX += this.velocityX;
+        this.positionY += this.velocityY;
+    }
+
+    scalarVelocity() {
+        return Math.sqrt(Math.pow(this.velocityX, 2) + Math.pow(this.velocityY, 2));
+    }
+
+    movementAngle() {
+        var angle = Math.atan2(this.velocityY, this.velocityX);
+        //angle = angle < 0 ? angle + Math.PI * 2 : angle;
+        return angle;
+    }
+
+    checkWallCollision() {
+        if (this.positionY - this.radius <= 0) {
+            this.velocityY = -this.velocityY;
+        }
+
+        // floor bounce
+        if (this.positionY + this.radius >= canvas.height) {
+            this.velocityY = -this.velocityY;
+        }
+
+        // left wall bounce
+        if (this.positionX - this.radius <= 0) {
+            this.velocityX = -this.velocityX;
+        }
+
+        // right wall bounce
+        if (this.positionX + this.radius >= canvas.width) {
+            this.velocityX = -this.velocityX;
+        }
+    }
+
+    resolveBallCollision(otherBall) {
+        this.positionX -= this.velocityX;
+        this.positionY -= this.velocityY;
+        otherBall.positionX -= otherBall.velocityX;
+        otherBall.positionY -= otherBall.velocityY;
+
+        let contactAngle = Math.atan2(otherBall.positionY - this.positionY, otherBall.positionX - this.positionX);
+        //contactAngle = contactAngle < 0 ? contactAngle + Math.PI*2 : contactAngle;
+        let v1 = this.scalarVelocity();
+        let v2 = otherBall.scalarVelocity();
+        let m1 = this.mass;
+        let m2 = otherBall.mass;
+        let theta1 = this.movementAngle();
+        let theta2 = otherBall.movementAngle();
+
+        let vx1F = (v1 * Math.cos(theta1 - contactAngle) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - contactAngle)) / (m1 + m2) * Math.cos(contactAngle) + v1 * Math.sin(theta1 - contactAngle) * Math.cos(contactAngle + Math.PI / 2);
+        let vy1F = (v1 * Math.cos(theta1 - contactAngle) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - contactAngle)) / (m1 + m2) * Math.sin(contactAngle) + v1 * Math.sin(theta1 - contactAngle) * Math.sin(contactAngle + Math.PI / 2);
+
+        let vx2F = (v2 * Math.cos(theta2 - contactAngle) * (m2 - m1) + 2 * m2 * v1 * Math.cos(theta1 - contactAngle)) / (m2 + m1) * Math.cos(contactAngle) + v2 * Math.sin(theta2 - contactAngle) * Math.cos(contactAngle + Math.PI / 2);
+        let vy2F = (v2 * Math.cos(theta2 - contactAngle) * (m2 - m1) + 2 * m2 * v1 * Math.cos(theta1 - contactAngle)) / (m2 + m1) * Math.sin(contactAngle) + v2 * Math.sin(theta2 - contactAngle) * Math.sin(contactAngle + Math.PI / 2);
+
+        this.velocityX = vx1F;
+        this.velocityY = vy1F;
+
+        otherBall.velocityX = vx2F;
+        otherBall.velocityY = vy2F;
+
+        
+    }
+
+    hasBallCollide(otherBall) {
+        
+        if (this.distanceTo(otherBall) <= this.radius + otherBall.radius){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    distanceTo(otherBall) {
+        let dx = otherBall.positionX - this.positionX;
+        let dy = otherBall.positionY - this.positionY;
+        let dist = Math.sqrt(dx * dx + dy * dy);
+        return dist;
+    }
+
+    isOverlapping(otherBall) {
+        if (this.distanceTo(otherBall) < this.radius + otherBall.radius) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 var canvas, balls = [], ctx;
 var colors = ["#f2a154", "#bbdfc8", "#ad6c80", "#c1a1d3", "#939b62"]
-
+var startTime = -1;
+var animationLength = 999999;
 
 
 function init() {
@@ -61,121 +62,48 @@ function init() {
     
 }
 
-function animation() {
+function animation(timestamp) {
+    var progress = 0;
+
+    if (startTime < 0) {
+        startTime = timestamp;
+    } else {
+        progress = timestamp - startTime;
+    }
     
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
 
     balls.forEach(e => {
         e.move(); 
     });
 
-    for (let i = 0; i < balls.length; i++) {
-        for (let j = i + 1; j < balls.length; j++) {
+    for (i = 0; i < balls.length; i++) {
+        let b = balls[i];
+        b.checkWallCollision();
+    }
+
+    for (i = 0; i < balls.length; i++) {
+        for (j = i + 1; j < balls.length; j++) {
             let b1 = balls[i];
             let b2 = balls[j];
-            if (b1 != b2) {
-                let dist = Math.sqrt(Math.pow(b2.positionX - b1.positionX, 2) + Math.pow(b2.positionY - b1.positionY, 2));
-                if (dist < b1.radius + b2.radius) {
-                
-                    b1.ballCollision(b2);
-                }
+            if (b1.hasBallCollide(b2)) {
+                b1.resolveBallCollision(b2);
             }
         }
-        
-    }      
+    } 
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     balls.forEach(e => {
         e.show(); 
     });
 
-    window.requestAnimationFrame(animation);
-}
-
-class Ball {
-    constructor(positionX, positionY, radius, velocityX, velocityY, mass, color) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.radius = radius;
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
-        this.mass = mass;
-        this.color = color;
-    }
-
-    show() {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.restore();
-    }
-
-    move() {
-        this.wallCollision();
-                
-        // position update
-        this.positionX += this.velocityX;
-        this.positionY += this.velocityY;
-    }
-
-    scalarVelocity() {
-        return Math.sqrt(Math.pow(this.velocityX, 2) + Math.pow(this.velocityY, 2));
-    }
-
-    movementAngle() {
-        var angle = Math.atan2(this.velocityY, this.velocityX);
-        //angle = angle < 0 ? angle + Math.PI * 2 : angle;
-        return angle;
-    }
-
-    wallCollision() {
-        if (this.positionY - this.radius <= 0) {
-            this.velocityY = -this.velocityY;
-        }
-
-        // floor bounce
-        if (this.positionY + this.radius >= canvas.height) {
-            this.velocityY = -this.velocityY;
-        }
-
-        // left wall bounce
-        if (this.positionX - this.radius <= 0) {
-            this.velocityX = -this.velocityX;
-        }
-
-        // right wall bounce
-        if (this.positionX + this.radius >= canvas.width) {
-            this.velocityX = -this.velocityX;
-        }
-    }
-
-    ballCollision(otherBall) {
-        this.positionX -= this.velocityX;
-        this.positionY -= this.velocityY;
-        otherBall.positionX -= otherBall.velocityX;
-        otherBall.positionY -= otherBall.velocityY;
-        let contactAngle = Math.atan2(otherBall.positionY - this.positionY, otherBall.positionX - this.positionX);
-        //contactAngle = contactAngle < 0 ? contactAngle + Math.PI*2 : contactAngle;
-        let v1 = this.scalarVelocity();
-        let v2 = otherBall.scalarVelocity();
-        let m1 = this.mass;
-        let m2 = otherBall.mass;
-        let theta1 = this.movementAngle();
-        let theta2 = otherBall.movementAngle();
-
-        let vx1F = (v1 * Math.cos(theta1 - contactAngle) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - contactAngle)) / (m1 + m2) * Math.cos(contactAngle) + v1 * Math.sin(theta1 - contactAngle) * Math.cos(contactAngle + Math.PI / 2);
-        let vy1F = (v1 * Math.cos(theta1 - contactAngle) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - contactAngle)) / (m1 + m2) * Math.sin(contactAngle) + v1 * Math.sin(theta1 - contactAngle) * Math.sin(contactAngle + Math.PI / 2);
-
-        let vx2F = (v2 * Math.cos(theta2 - contactAngle) * (m2 - m1) + 2 * m2 * v1 * Math.cos(theta1 - contactAngle)) / (m2 + m1) * Math.cos(contactAngle) + v2 * Math.sin(theta2 - contactAngle) * Math.cos(contactAngle + Math.PI / 2);
-        let vy2F = (v2 * Math.cos(theta2 - contactAngle) * (m2 - m1) + 2 * m2 * v1 * Math.cos(theta1 - contactAngle)) / (m2 + m1) * Math.sin(contactAngle) + v2 * Math.sin(theta2 - contactAngle) * Math.sin(contactAngle + Math.PI / 2);
-
-        this.velocityX = vx1F;
-        this.velocityY = vy1F;
-
-        otherBall.velocityX = vx2F;
-        otherBall.velocityY = vy2F;
+    if (progress < animationLength) {
+        requestAnimationFrame(animation);
     }
 }
+
+
 
 init();
 window.requestAnimationFrame(animation);
